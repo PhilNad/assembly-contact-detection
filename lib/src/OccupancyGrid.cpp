caa3c370ddf18f3b8aedeb4301dd8023fd56d56d
PhilNad <philippe.nadeau1991@gmail.com>
2023-09-27 16:18:09 -0400

    Improved sampling on triangles

--- lib/src/OccupancyGrid.cpp
+++ lib/src/OccupancyGrid.cpp
@@ -92,6 +92,7 @@ bool point_inside_triangle(const Vector3f& p, const Vector3f& v0, const Vector3f
     float alpha = (d11 * d20 - d01 * d21) * inv_denom;
     float beta  = (d00 * d21 - d01 * d20) * inv_denom;
     float gamma = 1.0f - alpha - beta;
+    cout << "alpha = " << alpha << ", beta = " << beta << ", gamma = " << gamma << endl;
     //Check if point is inside triangle
     return ((0 <= alpha+tol) && (alpha-tol <= 1) &&
             (0 <= beta+tol)  && (beta-tol  <= 1) &&
@@ -113,66 +114,142 @@ vector<Vector3f> OccupancyGrid::sample_points_in_triangle(const Vector3f& v0, co
     //OccupancyGrid should be initialized
     assert(this->cell_size[0] > 1e-12 && this->cell_size[1] > 1e-12 && this->cell_size[2] > 1e-12);
 
-    //Express vertices relative to v0
-    Vector3f v0r;
-    v0r << 0, 0, 0;
-    Vector3f v1r = v1 - v0;
-    Vector3f v2r = v2 - v0;
+    //Get the lengths of each side of the triangle
+    float side_length_0 = (v1 - v0).norm();
+    float side_length_1 = (v2 - v1).norm();
+    float side_length_2 = (v0 - v2).norm();
 
-    //Project the triangle onto the x-y plane
-    Vector3f normal = v1r.cross(v2r).normalized();
-    Vector3f x_axis = v1r.normalized();
-    Vector3f y_axis = normal.cross(x_axis).normalized();
-    //Rotation matrix that projects points onto the x-y plane
-    Matrix3f projection = (MatrixX3f(3, 3) << x_axis, y_axis, normal).finished();
-    //Matrix3f is initialized column by column and needs to be transposed to get the desired rotation matrix
-    projection.transposeInPlace();
-    //Each column is the coordinates of a vertex
-    Matrix3f vertices_in_columns = (MatrixX3f(3, 3) << v0r, v1r, v2r).finished();
-    //Each row is the coordinates of a vertex projected onto the x-y plane
-    Matrix3f projected_vertices = projection * vertices_in_columns;
+    //Set the origin as the vertex in contact with the two longest sides
+    Vector3f origin;
+    uint32_t origin_idx;
+    if(side_length_0 < side_length_1 && side_length_0 < side_length_2){
+        //Side 0 is the shortest, v2 is the origin
+        origin << v2;
+        origin_idx = 2;
+    }else if(side_length_1 < side_length_0 && side_length_1 < side_length_2){
+        //Side 1 is the shortest, v0 is the origin
+        origin << v0;
+        origin_idx = 0;
+    }else if(side_length_2 < side_length_0 && side_length_2 < side_length_1){
+        //Side 2 is the shortest, v1 is the origin
+        origin << v1;
+        origin_idx = 1;
+    }
+
+    //Express vertices relative to the origin
+    Vector3f v0r = v0 - origin;
+    Vector3f v1r = v1 - origin;
+    Vector3f v2r = v2 - origin;
+
+    //Define the X axis as the one with the longest side of the triangle
+    Vector3f x_axis, y_axis, normal;
+    if(origin_idx == 0){
+        if(v1r.norm() > v2r.norm()){
+            //Longest edge goes from v0 to v1, its the x-axis
+            x_axis = v1r.normalized();
+            normal = x_axis.cross(v2r).normalized();
+            y_axis = normal.cross(x_axis);
+        }else{
+            //Longest edge goes from v0 to v2
+            x_axis = v2r.normalized();
+            normal = x_axis.cross(v1r).normalized();
+            y_axis = normal.cross(x_axis);
+        }
+    }else if(origin_idx == 1){
+        if(v0r.norm() > v2r.norm()){
+            //Longest edge goes from v1 to v0
+            x_axis = v0r.normalized();
+            normal = x_axis.cross(v2r).normalized();
+            y_axis = normal.cross(x_axis);
+        }else{
+            //Longest edge goes from v1 to v2
+            x_axis = v2r.normalized();
+            normal = x_axis.cross(v0r).normalized();
+            y_axis = normal.cross(x_axis);
+        }
+    }else if(origin_idx == 2){
+        if(v0r.norm() > v1r.norm()){
+            //Longest edge goes from v2 to v0
+            x_axis = v0r.normalized();
+            normal = x_axis.cross(v1r).normalized();
+            y_axis = normal.cross(x_axis);
+        }else{
+            //Longest edge goes from v2 to v1
+            x_axis = v1r.normalized();
+            normal = x_axis.cross(v0r).normalized();
+            y_axis = normal.cross(x_axis);
+        }
+    }
+
+    //Transformation that projects points in the world frame onto the x-y plane
+    // such that the origin is the vertex in contact with the two longest sides
+    // and the x-axis is the longest side of the triangle.
+    Matrix3f rotation;
+    rotation.row(0) = x_axis;
+    rotation.row(1) = y_axis;
+    rotation.row(2) = normal;
+    Affine3f transfo = Affine3f::Identity();
+    transfo.linear()        = rotation;
+    transfo.translation()   = -rotation*origin;
+    //Also define the inverse transformation
+    Affine3f inv_transfo = Affine3f::Identity();
+    inv_transfo.linear()        = rotation.transpose();
+    inv_transfo.translation()   = rotation.transpose()*origin;
+
+    //Express vertices in the newly defined frame
+    Matrix3f vertices_in_columns = (MatrixX3f(3, 3) << v0, v1, v2).finished();
+    //Each column is the coordinates of a vertex projected onto the x-y plane
+    Matrix3f projected_vertices = transfo * vertices_in_columns;
     Vector3f v0_proj = projected_vertices.col(0);
     Vector3f v1_proj = projected_vertices.col(1);
     Vector3f v2_proj = projected_vertices.col(2);
 
     //Get the bounding box of the projected triangle
-    float min_x = projected_vertices.row(0).minCoeff();
-    float min_y = projected_vertices.row(1).minCoeff();
     float max_x = projected_vertices.row(0).maxCoeff();
     float max_y = projected_vertices.row(1).maxCoeff();
 
-    //Side length of each grid cell
-    // Note: The side lengths can be zero if the axis is orthogonal to (1,1,1)
-    float side_length_x = abs(x_axis.dot(this->cell_size));
-    float side_length_y = abs(y_axis.dot(this->cell_size));
+    //Project the voxel diagonal onto the x-y plane
+    Vector3f diag_proj = rotation * this->cell_size;
 
-    //This is done to avoid issues with very small side lengths
-    if(side_length_x < this->cell_size[0] / 10)
-        side_length_x = this->cell_size[0];
+    //Step size
+    //TODO: Should this change depending on the orientation of the triangle?
+    float step_size_x = this->cell_size[0] / 5;
+    float step_size_y = this->cell_size[0] / 5;
 
-    if(side_length_y < this->cell_size[1] / 10)
-        side_length_y = this->cell_size[1];
-
-    assert(side_length_x > 0);
-    assert(side_length_y > 0);
-
-    cout << "Will execute " << ((max_x-min_x)/side_length_x) * ((max_y-min_y)/side_length_y) << " point checks." << endl;
+    assert(step_size_x > 0);
+    assert(step_size_y > 0);
 
     //Sample points in the bounding box
     vector<Vector3f> points;
-    for (float x = min_x; x <= max_x; x += side_length_x)
+    int saved_checks = 0;
+    for (float x = 0; x <= max_x; x += step_size_x)
     {
-        for (float y = min_y; y <= max_y; y += side_length_y)
+        //Tracks when we enter and leave the triangle
+        bool prev_point_inside = false;
+        for (float y = 0; y <= max_y; y += step_size_y)
         {
-            //Check if point is inside triangle
-            if (point_inside_triangle(Vector3f(x, y, 0), v0_proj, v1_proj, v2_proj))
+            //Check if the (x,y) point is inside triangle.
+            // Our reference frame fixed in the triangle guarantees that any point with y=0
+            // will be inside the triangle, saving a few more costly checks.
+            if (y == 0 || point_inside_triangle(Vector3f(x, y, 0), v0_proj, v1_proj, v2_proj))
             {
-                //Here we are using the fact the the inverse is the transpose for a proper rotation matrix
-                Vector3f unprojected_point = projection.transpose() * Vector3f(x, y, 0) + v0;
+                //Use the inverse transformation to get the point in the world frame
+                Vector3f unprojected_point = inv_transfo.linear() * Vector3f(x, y, 0) + inv_transfo.translation();
                 points.push_back(unprojected_point);
+                prev_point_inside = true;
+                cout << "Coord (" << x << ", " << y << ") is INSIDE the triangle." << endl;
+            }else{
+                cout << "Coord (" << x << ", " << y << ") is OUTSIDE the triangle." << endl;
+                if(prev_point_inside){
+                    //We are leaving the triangle, no need to check more points
+                    // as the triangle is convex.
+                    saved_checks += ceil((max_y - y)/step_size_y) + 1;
+                    break;
+                }
             }
         }
     }
+    cout << "Saved " << saved_checks << "/" << points.size()+saved_checks << " checks." << endl;
     return points;
 }
 
@@ -339,6 +416,13 @@ OccupancyGrid::OccupancyGrid(const MatrixX3f& vertices, const MatrixX3i& triangl
         // Get triangle normal assuming that the indices are
         // ordered with the PhysX convention.
         Vector3f normal = (v1 - v0).cross(v2 - v0).normalized();
+
+        //TEST TO REMOVE AFTER USE
+        Vector3f t0, t1, t2;
+        t0 << 0, 0, 1;
+        t1 << 0.5, 0, 1;
+        t2 << -0.25, 0.25, 1;
+        sample_points_in_triangle(t0, t1, t2);
 
         // Sample points in the triangle
         vector<Vector3f> sampled_points = sample_points_in_triangle(v0, v1, v2);
